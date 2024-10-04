210cfadec67e65923baca3c60d53a171e877c3b2
Jake Boateng <64sjjsi0@students.codeinstitute.net>
2024-10-04 17:55:39 +0000

    created add_task.html template to trigger the form to submit via the add_task function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,3 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
   });
+
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {done: "Select"}
+
+
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
